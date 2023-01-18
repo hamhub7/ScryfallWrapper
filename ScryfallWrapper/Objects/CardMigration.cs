651981f738e4cf3f17d07bf372f8361fdfff9683
hamhub7 <hamhub7@yahoo.com>
2023-01-18 11:20:56 -0500

    CardMigration object created, CardMigrations updated to use error checking

---
+++ ScryfallWrapper/Objects/CardMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScryfallWrapper.Objects
+{
+    public class CardMigration
+    {
+        /// <summary>
+        /// A link to the current object on Scryfall’s API.
+        /// </summary>
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// This migration’s unique UUID.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The date this migration was performed.
+        /// </summary>
+        public string CreatedAt { get; set; }
+
+        /// <summary>
+        /// A computer-readable indicator of the migration strategy. See above.
+        /// </summary>
+        public string MigrationStrategy { get; set; }
+
+        /// <summary>
+        /// The id of the affected API Card object.
+        /// </summary>
+        public string OldScryfallId { get; set; }
+
+        /// <summary>
+        /// The replacement id of the API Card object if this is a merge.
+        /// </summary>
+        public string? NewScryfallId { get; set; }
+
+        /// <summary>
+        /// A note left by the Scryfall team about this migration.
+        /// </summary>
+        public string? Note { get; set; }
+    }
+}
--- ScryfallWrapper/Requests/CardMigrations.cs
+++ ScryfallWrapper/Requests/CardMigrations.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using ScryfallWrapper.Errors;
+using ScryfallWrapper.Objects;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -17,7 +20,10 @@ namespace ScryfallWrapper.Requests
         /// This method is paginated, returning 350 objects at a time.Review the documentation for paginating the List type to understand all of the possible output from this method.
         /// </summary>
         /// <param name="page">The page number to return.</param>
-        public async Task<HttpResponseMessage> GetMigrations(int page)
+        /// <exception cref="ScryfallException">Thrown when the API returns an error</exception>
+        /// <exception cref="NullReferenceException">Thrown when a cast fails</exception>
+        /// <exception cref="Exception">Thrown when an unexpected type is received</exception>
+        public async Task<ScryList<CardMigration>> GetMigrations(int page)
         {
             NameValueCollection query = HttpUtility.ParseQueryString("");
             query["page"] = page.ToString();
@@ -25,21 +31,61 @@ namespace ScryfallWrapper.Requests
             {
                 Query = query.ToString()
             };
-            return await _httpClient.GetAsync(builder.Uri);
+
+            HttpResponseMessage response = await _httpClient.GetAsync(builder.Uri);
+            string json = await response.Content.ReadAsStringAsync();
+            JObject jObject = JObject.Parse(json);
+            string? objectValue = (string?)jObject["object"];
+
+            if (objectValue == "list")
+            {
+                // TODO: double check this guy
+                ScryList<CardMigration>? list = jObject.ToObject<ScryList<CardMigration>>();
+                return list ?? throw new NullReferenceException();
+            }
+            else if (objectValue == "error")
+            {
+                throw jObject.ToObject<ScryfallException>() ?? throw new NullReferenceException();
+            }
+            else
+            {
+                throw new Exception($"Unexpected type received: {objectValue}");
+            }
         }
 
         /// <summary>
         /// Returns a single Card Migration with the given :id
         /// </summary>
         /// <param name="id">The ID of the migration.</param>
-        public async Task<HttpResponseMessage> GetMigrationsId(string id)
+        /// <exception cref="ScryfallException">Thrown when the API returns an error</exception>
+        /// <exception cref="NullReferenceException">Thrown when a cast fails</exception>
+        /// <exception cref="Exception">Thrown when an unexpected type is received</exception>
+        public async Task<CardMigration> GetMigrationsId(string id)
         {
             NameValueCollection query = HttpUtility.ParseQueryString("");
             UriBuilder builder = new(ENDPOINT + CARD_MIGRATIONS + $"/{id}")
             {
                 Query = query.ToString()
             };
-            return await _httpClient.GetAsync(builder.Uri);
+
+            HttpResponseMessage response = await _httpClient.GetAsync(builder.Uri);
+            string json = await response.Content.ReadAsStringAsync();
+            JObject jObject = JObject.Parse(json);
+            string? objectValue = (string?)jObject["object"];
+
+            if (objectValue == "migration")
+            {
+                CardMigration? migration = jObject.ToObject<CardMigration>();
+                return migration ?? throw new NullReferenceException();
+            }
+            else if (objectValue == "error")
+            {
+                throw jObject.ToObject<ScryfallException>() ?? throw new NullReferenceException();
+            }
+            else
+            {
+                throw new Exception($"Unexpected type received: {objectValue}");
+            }
         }
     }
 }
